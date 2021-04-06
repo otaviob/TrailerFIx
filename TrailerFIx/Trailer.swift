bc8cc438c882d75c52e6c96bfe127167b2485536
otavio brito <otaviob22@hotmail.com>
2021-04-06 14:11:31 -0300

    first commit

---
+++ TrailerFIx/Trailer.swift
@@ -0,0 +1,15 @@
+//
+//  Trailer.swift
+//  TrailerFIx
+//
+//  Created by Otavio Brito on 12/11/20.
+//
+import Foundation
+
+struct Trailer: Codable {
+    let title: String
+    let url: String
+    let rating: Int
+    let year: Int
+    let poster: String
+}
